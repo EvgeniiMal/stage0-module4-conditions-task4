a087357121a84f71aea444cdd0a63b309a5dd139
Eugene <60567002+EvgeniiMal@users.noreply.github.com>
2022-11-14 16:07:29 +0100

    implement task

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int gr_one = first > second && first > third ? first : second;
+        int gr_two = gr_one > third ? gr_one : third;
+        System.out.println(gr_two);
     }
 }
